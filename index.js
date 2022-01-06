3b59e5df592c6b80bd1e9ba3ba63b1b138b59da1
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-06 17:06:28 +0200

    add speakers images and descriptions

--- index.js
+++ index.js
@@ -15,54 +15,38 @@ closetog.addEventListener('click', close);
 
 document.querySelectorAll('.menu-list').forEach((link) => link.addEventListener('click', close));
 
-
 const data = [{
   id: 0,
-  name: 'Bilal Çinarli',
-  image: 'images/speaker1.png',
-  description: 'Bilal is Head of Frontend Architecture for adidas and Google Developer Expert on Web Technologies. He focuses on performance, optimization and code scalability.',
-  role: 'Frontend Web Developer',
+  name: 'Moritz Baier-Lentz',
+  image: './images/speaker1.jpg',
+  description: 'Moritz Baier-Lentz is the Partner at BITKRAFT Ventures. Additionally, Moritz Baier-Lentz has had 3 past jobs including Vice President and Co-Head of Gaming and Esports at Goldman Sachs.',
+  role: 'Investment Partner',
 },
 {
   id: 1,
-  name: 'Christian Liebel',
-  image: 'images/speaker2.png',
-  description: 'Christian Liebel is a cross-platform development enthusiast thrilled by the opportunities offered by modern web technologies.',
-  role: 'cross-platform development enthusiast',
+  name: 'Jamison Selby',
+  image: './images/speaker2.jpg',
+  description: 'Jamison Selby is a dynamic, entrepreneurial, customer-focused, Gaming executive with many years’ experience leading cross-disciplinary design and production teams in the creation, marketing and live operation of multiplayer games and interactive entertainment',
+  role: 'SVP, Games & Marketing',
 },
 {
   id: 2,
-  name: 'Joel Lord',
-  image: 'images/speaker3.png',
-  description: 'Joel Lord is passionate about the web and technology in general. He likes to learn new things, but most of all, he wants to share his discoveries.',
-  role: 'Developer advocate with MongoDB',
+  name: 'Zai Ortiz',
+  image: './images/speaker3.jpg',
+  description: 'Zai Ortiz is the visionary entrepreneur-turned game maker who sits behind the wheel of multiple million-dollar creative projects. With a 20 year career defined by cutting-edge technology – whether in Hollywood visual effects or interactive game development',
+  role: 'visionary entrepreneur',
 },
 {
   id: 3,
-  name: 'Tamar Stern',
-  image: 'images/speaker4.png',
-  description: 'She has a decade of software engineering experience in various technologies: Server side, big data, mobile, web technologies, and security.',
-  role: 'Full-stack Software Developer',
-},
-{
-  id: 4,
-  name: 'Anuradha Kumari',
-  image: 'images/speaker5.png',
-  description: 'Anuradha is a frontend developer passionate about exploring new technologies and sharing knowledge through tech articles and talks.',
-  role: 'Frontend Web Developer',
-},
-{
-  id: 5,
-  name: 'Syneva Runyan',
-  image: 'images/speaker6.png',
-  description: 'Syneva is an engineering manager based out of Atlanta, Georgia. She is passionate about people, processes, and JavaScript-powered projects.',
-  role: 'Software engineer at MailChimp',
+  name: 'Mariusz Gasiewski',
+  image: './images/speaker4.jpg',
+  description: 'Mariusz Gasiewski has 2 current jobs as CEE Mobile Gaming Lead at Google and Head of Mobile Gaming and Apps Poland at Google. Additionally, Mariusz Gasiewski has had 5 past jobs including CEE Performance Activation Lead at Google.',
+  role: ' CEE Mobile Gaming Lead',
 },
 ];
 
-
 const cards = document.querySelector('.speakers');
-for (let i = 0; i < data.length; i ++) {
+for (let i = 0; i < data.length; i += 1) {
   const Card = document.createElement('div');
   Card.className = 'card';
   const datas = `
